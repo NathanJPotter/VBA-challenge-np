c06c35b08bb98ae4c8e86e538b86aaa76568a3b1
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-05-16 17:35:50 +1000

    Create Sub Percentage_Change().vb
    The percentage change works. But I have a bug associated with a counter insider a nested loop: "Ticker_row_start = Ticker_row_start +1".
    It creates a ""Run-time error '6': Overflow" error.
    The error doesn't happen if I run the two loops consecutively (and not as one loop nested inside the other). But then my values are wrong.

---
+++ Sub Percentage_Change().vb
@@ -0,0 +1,143 @@
+Sub Percentage_Change()
+
+   
+   ' Here is a script that will loop through all the stocks for one year and output:
+    ' - The ticker symbol
+    ' - Yearly change from opening at the begining of a given year to the closing price at the end of the year
+        ' - including conditional formatting that highlights positive change in green and negative change in red.
+     ' - The percentage change from opening at the begining of a given year to the closing price at the end of the year
+
+    ' Add title and format the Ticker column
+
+     Cells(1, 10).Value = "Ticker"
+
+     Cells(1, 10).Font.Bold = True
+
+    ' Add title and format to the Yearly Change column
+
+    Cells(1, 11).Value = "Yearly Change"
+    Cells(1, 11).Font.Bold = True
+
+    ' Add titles and format to year open value and year close close culumns
+
+    Cells(1, 15).Value = "Year Open"
+    Cells(1, 15).Font.Bold = True
+
+    Cells(1, 16).Value = "Year Close"
+    Cells(1, 16).Font.Bold = True
+
+     ' Add title and format to the Yearly Change column
+
+    Cells(1, 11).Value = "Yearly Change"
+    Cells(1, 11).Font.Bold = True
+
+    ' Add title and format to the Percentage Change column
+
+    Cells(1, 12).Value = "Percentage Change"
+    Cells(1, 12).Font.Bold = True
+
+    ' Add title and format to the Total Stock Volume column
+
+    Cells(1, 13).Value = "Total Stock Volume"
+    Cells(1, 13).Font.Bold = True
+
+    'Keep track of the location for each stock/ticker type
+     Dim Ticker_row_finish As Integer
+     Ticker_row_finish = 2
+
+     Dim Ticker_row_start As Integer
+     Ticker_row_start = 2
+
+
+     ' Define variables for yearly change
+
+    Dim close_value As Double
+    Dim open_value As Double 
+    Dim yearly_change As Double
+    Dim percentage_change As Double
+    Dim total_stock As Double
+    Dim close_volume As Double
+    Dim open_volume As Double
+
+    
+    ' Set numrows = number of rows of data.
+
+        numrows = Range("A1", Range("A1").End(xlDown)).Rows.Count
+
+    'Loop through all the stock/ticker types with a For loop to loop "numrows" number of times
+
+     For i = 2 to numrows
+
+          If Cells( i + 1, 1).Value <> Cells(i, 1).Value Then
+
+         ' Set the stock/ticker name
+
+            Ticker_name = Cells(i, 1).Value
+
+         ' Print the stock/ticker name in the Ticker row
+
+            Range("J" & Ticker_row_finish).Value = Ticker_name
+
+            ' Find the last date
+
+            close_value = Cells(i, 6).Value
+
+            close_volume = Cells(i, 7).Value
+
+            Range("P" & Ticker_row_finish).Value = close_value
+
+                ' Find the first date
+
+                 For j = 2 to numrows
+
+                     ' Define start of the date range
+                      If Cells( j - 1, 1).Value <> Cells(j, 1).Value Then
+
+                         open_value = Cells(j, 3).Value
+
+                         open_volume = Cells(j, 7).Value
+
+                         Range("O" & Ticker_row_start).Value = open_value
+
+                        ' Calculate the value of yearly change as close - open
+
+                            yearly_change = close_value - open_value
+
+                        ' Print Yearly_Change in the Yearly Change column
+
+                            Range("K" & Ticker_row_start).Value = yearly_change
+
+                        ' Calculate the value of percentage change
+
+                            percentage_change = (yearly_change / open_value) * 100
+
+                        'Print percentage_change in Percentage Change column
+
+                            Range("L" & Ticker_row_start).Value = percentage_change
+                        
+                        ' Calculate the value of Total Stock volume
+
+                            total_stock = WorksheetFunction.SUM(Range(Cells(i, 7):Cells(j, 7)))
+
+                        'Print total_stock in Total Stock column
+
+                            Range("M" & Ticker_row_start).Value = total_stock
+
+                            ' Add one to ticker row start
+
+                            Ticker_row_start = Ticker_row_start + 1
+
+                        
+                     End If
+
+                Next j
+
+             ' Add one to ticker row finish
+
+             Ticker_row_finish = Ticker_row_finish + 1
+
+         End If
+
+    Next i    
+         
+End Sub
